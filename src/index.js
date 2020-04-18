ae399aecbde66460f895089e179e0126ace40c68
bhavBains <deepbains26@gmail.com>
2020-04-18 12:12:23 -0700

    component nesting and component reuse

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sammie
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-      </div>
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 2:00AM"
+          post="Nice post"
+        />
+      </ApprovalCard>
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 6:00PM"
+        post="Yayy Thats kool"
+      />
+      <CommentDetail author="Jane" timeAgo="Today at 11:00AM" post="Cooool" />
     </div>
   );
 };
